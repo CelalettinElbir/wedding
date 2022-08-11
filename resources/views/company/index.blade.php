764f99856eb648d6d38fe8b44c82584a485d5d9e
Celalettin Elbir <celallelbir@hotmail.com>
2022-08-12 01:40:49 +0300

    detail-page

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,20 +1,48 @@
 @extends('layout')
 
 @section('content')
-    @foreach ($data as $item)
-        <div class="col-md-6 mx-auto mt-4">
-            <div class="card" style="border:2px solid #f5d0fe">
-                <div class="card-body d-flex justify-content-between">
-                    <div class="">
-                        <h5 class="card-title">{{ $item->name }}</h5>
-                        <p class="card-text">{{ $item->telno }}</p>
-                    </div>
-                    <div>
-                        <a class="btn btn-light"  href="/company/{{ $item->id }} ">paketleri göster</a>
+    <div class="company-container ">
+
+
+        @foreach ($data as $item)
+            <div class="col-md-6 mx-auto m-4 border border-primary d-flex p-1">
+                <div class="company-img m-3">
+                    <img src="{{ asset('/' . $item->takeimages()->first()->url) }}" style=" width:200px" class="'mg" />
+
+                </div>
+                <div class="company-body">
+                    <h3>{{ $item->company_name }}</h3>
+
+
+
+                    @if (strlen($item->description) > 300)
+                        <p>{{ substr($item->description, 0, 150) }} ...read more</p>
+                    @else
+                        <p>{{ $item->description }}</p>
+                    @endif
+
+                    {{-- <p>{{ $item->description }}</p> --}}
+
+                    <div class="company-footer d-flex justify-content-between">
+
+                        <div class="d-flex">
+                            <p class="m-3">{{ $item->price }}</p>
+                            <p class="m-3">{{ $item->capasity }}</p>
+                            <p class="m-3">{{ $item->telno }}</p>
+
+                        </div>
+
+                        <div>
+
+                            <a class="btn btn-primary m-2 float-right" href="company/{{ $item->id }}">detaylar</a>
+
+                        </div>
 
                     </div>
                 </div>
+
+
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 @endsection
